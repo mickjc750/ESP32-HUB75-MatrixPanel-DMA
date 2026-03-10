7220dd1ff097b9ec9015a850a6e723ffc24c999f
mick <michael.j.clift@gmail.com>
2026-03-11 09:53:47 +1100

    invert scan

--- src/ESP32-HUB75-MatrixPanel-I2S-DMA.cpp
+++ src/ESP32-HUB75-MatrixPanel-I2S-DMA.cpp
@@ -500,7 +500,7 @@ void MatrixPanel_I2S_DMA::clearFrameBuffer(bool _buff_id)
     ESP32_I2S_DMA_STORAGE_TYPE abcde = (ESP32_I2S_DMA_STORAGE_TYPE)row_idx;
     abcde <<= BITS_ADDR_OFFSET; // shift row y-coord to match ABCDE bits in vector from 8 to 12
     #ifdef HUB75_INVERT_SCAN
-      abcde ^= 0x1F << BITS_ADDR_OFFSET
+      abcde ^= 0x1F << BITS_ADDR_OFFSET;
     #endif
     // get last pixel index in a row of all colourdepths
     int x_pixel = fb->rowBits[row_idx]->width * fb->rowBits[row_idx]->colour_depth;
@@ -532,7 +532,7 @@ void MatrixPanel_I2S_DMA::clearFrameBuffer(bool _buff_id)
     //  previous row while we pump in LSB's for a new row
     abcde = ((ESP32_I2S_DMA_STORAGE_TYPE)row_idx - 1) << BITS_ADDR_OFFSET;
     #ifdef HUB75_INVERT_SCAN
-      abcde ^= 0x1F << BITS_ADDR_OFFSET
+      abcde ^= 0x1F << BITS_ADDR_OFFSET;
     #endif
     do
     {
